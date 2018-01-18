e226f3924d1b9c2cc8379345affc30e9b8f0b03b
alchemistmatt <matthew.monroe@pnnl.gov>
2018-01-18 14:10:39 -0800

    Update standard header

--- DataModules/BarPlot.cs
+++ DataModules/BarPlot.cs
@@ -1,21 +1,13 @@
 ﻿/* Written by Joseph N. Brown
  * for the Department of Energy (PNNL, Richland, WA)
  * Battelle Memorial Institute
- * E-mail: joseph.brown@pnnl.gov
+ * E-mail: proteomics@pnnl.gov
  * Website: http://omics.pnl.gov/software
  * -----------------------------------------------------
  *
- * Notice: This computer software was prepared by Battelle Memorial Institute,
- * hereinafter the Contractor, under Contract No. DE-AC05-76RL0 1830 with the
- * Department of Energy (DOE).  All rights in the computer software are reserved
- * by DOE on behalf of the United States Government and the Contractor as
- * provided in the Contract.
- *
- * NEITHER THE GOVERNMENT NOR THE CONTRACTOR MAKES ANY WARRANTY, EXPRESS OR
- * IMPLIED, OR ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.
- *
- * This notice including this sentence must appear on any copies of this computer
- * software.
+ * Licensed under the Apache License, Version 2.0; you may not use this
+ * file except in compliance with the License.  You may obtain a copy of the
+ * License at https://opensource.org/licenses/Apache-2.0
  * -----------------------------------------------------*/
 
 using System;
